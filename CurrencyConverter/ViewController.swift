89fd466a493d3eebb5ed82a662bc942e752cbe35
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-04-15 15:11:08 +0900

    Initial Commit

---
+++ CurrencyConverter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by 서동환 on 4/15/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemBackground
+    }
+
+
+}
+

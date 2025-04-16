b5005e25e5d24e066c0476400f6ee850c2b2530c
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-04-16 10:50:45 +0900

    [Feat] #1 - 환율 데이터 UI 반영

--- CurrencyConverter/Presentation/ViewController/ViewController.swift
+++ CurrencyConverter/Presentation/ViewController/ViewController.swift
@@ -24,15 +24,13 @@ final class ViewController: UIViewController {
     
     // MARK: - UI Components
     
-    private lazy var currencyTableView = CurrencyTableView(frame: .zero).then {
-        $0.delegate = self
-        $0.dataSource = self
-    }
+    private lazy var currencyTableView = CurrencyTableView(frame: .zero)
     
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.navigationController?.navigationBar.isHidden = true
         self.view.backgroundColor = .systemBackground
         
         setupUI()
@@ -60,31 +58,23 @@ private extension ViewController {
     
     func bind() {
         viewModel.cellCount
-            .observe(on: MainScheduler.instance)
-            .subscribe(onNext: { [weak self] cellCount in
+            .asDriver(onErrorJustReturn: 0)
+            .drive(onNext: { [weak self] cellCount in
                 self?.cellCount = cellCount
             })
             .disposed(by: disposeBag)
-    }
-}
-
-// MARK: - UITableViewDelegate
-
-extension ViewController: UITableViewDelegate {
-    
-}
-
-// MARK: - UITableViewDataSource
-
-extension ViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return cellCount
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: CurrencyCell.identifier, for: indexPath)
         
-        return cell
+        viewModel.rates
+            .asDriver(onErrorJustReturn: [:])
+            .map({
+                return $0.sorted { $0.key < $1.key }
+            })
+            .drive(currencyTableView.rx.items(
+                cellIdentifier: CurrencyCell.identifier,
+                cellType: CurrencyCell.self)) { _, element, cell in
+                cell.configure(currencyCode: element.key, exchangeRate: element.value)
+            }
+            .disposed(by: disposeBag)
     }
 }
 
@@ -92,7 +82,7 @@ extension ViewController: UITableViewDataSource {
 
 struct Preview: PreviewProvider {
     static var previews: some View {
-        // {뷰 컨트롤러 이름}().toPreview()
+        // {뷰 컨트롤러 인스턴스}.toPreview()
         ViewController().toPreview()
     }
 }
--- CurrencyConverter/Presentation/ViewModel/CurrencyViewModel.swift
+++ CurrencyConverter/Presentation/ViewModel/CurrencyViewModel.swift
@@ -6,24 +6,21 @@
 //
 
 import Foundation
-import RxSwift
+import RxRelay
 
 final class CurrencyViewModel {
     
     // MARK: - Properties
     
     private let dataService = DataService()
-    private var rates: [String: Double]?
     
     // MARK: - Data ➡️ Output
     
-    var cellCount = BehaviorSubject<Int>(value: 0)
-    var loadCurrencyError = PublishSubject<Bool>()
+    var cellCount = BehaviorRelay<Int>(value: 0)
+    var loadCurrencyError = PublishRelay<Bool>()
     
-    /// 통화 코드
-    var currencyCodes = [BehaviorSubject<String>]()
-    /// 환율
-    var exchangeRates = [BehaviorSubject<Double>]()
+    /// [통화코드: 환율]
+    var rates = BehaviorRelay<[String: Double]>(value: [:])
     
     init() {
         loadCurrency()
@@ -37,19 +34,13 @@ private extension CurrencyViewModel {
             
             switch result {
             case .success(let currency):
-                rates = currency.rates
-                if let rates = rates {
-                    cellCount.onNext(rates.count)
-                    for rate in rates.sorted(by: { $0.key < $1.key }) {
-                        currencyCodes.append(BehaviorSubject(value: rate.key))
-                        exchangeRates.append(BehaviorSubject(value: rate.value))
-                    }
-                }
+                rates.accept(currency.rates)
+                cellCount.accept(rates.value.count)
                 
             case .failure(_):
-                rates = nil
-                cellCount.onNext(0)
-                loadCurrencyError.onNext(false)
+                rates.accept([:])
+                cellCount.accept(0)
+                loadCurrencyError.accept(true)
             }
         }
     }

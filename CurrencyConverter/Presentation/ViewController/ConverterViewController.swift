a6cb14cdc5062a32ac23d9fe9a1aa249c696e4cc
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-04-17 19:09:38 +0900

    [Fix] #3 - 커밋 미반영 수정

--- CurrencyConverter/Presentation/ViewController/ConverterViewController.swift
+++ CurrencyConverter/Presentation/ViewController/ConverterViewController.swift
@@ -19,6 +19,46 @@ final class ConverterViewController: UIViewController {
     
     // MARK: - UI Components
     
+    private let converterView = ConverterView()
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "환율 정보"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.view.backgroundColor = .systemBackground
+        
+        setupUI()
+    }
 }
 
-private extension Co
+private extension ConverterViewController {
+    func setupUI() {
+        setViewHierarchy()
+        setConstraints()
+    }
+    
+    func setViewHierarchy() {
+        self.view.addSubview(converterView)
+    }
+    
+    func setConstraints() {
+        converterView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+}
+
+// MARK: - PreviewProvider
+
+#if DEBUG
+import SwiftUI
+
+struct ConverterViewControllerPreview: PreviewProvider {
+    static var previews: some View {
+        // {뷰 컨트롤러 인스턴스}.toPreview()
+        ConverterViewController().toPreview()
+    }
+}
+#endif

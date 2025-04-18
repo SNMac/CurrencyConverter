6ad109e1e4456d809ebda1d45f3e6162535de2a2
DongHwan Seo <61683387+SNMac@users.noreply.github.com>
2025-04-19 03:33:55 +0900

    [Comment] #3 - 주석 작성

--- CurrencyConverter/Presentation/ViewModel/Protocol/ViewModelProtocol.swift
+++ CurrencyConverter/Presentation/ViewModel/Protocol/ViewModelProtocol.swift
@@ -8,9 +8,13 @@
 import Foundation
 
 protocol ViewModelProtocol {
+    /// ViewModel Output ➡️ State
     associatedtype Action
+    /// ViewModel Output ➡️ State
     associatedtype State
     
+    /// ViewModel에서 API 호출, 필터링, 환율 계산 등의 로직 처리 클로저 생성
     var action: ((Action) -> Void)? { get }
+    /// ViewController에서 클로저로 바인딩
     var state: State { get }
 }
